21df11d1bf16bade9697160499c8a7d241ef1354
Gilmara <gilmaralopes2006@gmail.com>
2022-11-09 20:37:46 -0300

    req03-pair programming

---
+++ src/pages/Settings.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Settings extends Component {
+  render() {
+    return (
+      <div data-testid="settings-title">
+        Configurações
+      </div>
+    );
+  }
+}
+export default Settings;
